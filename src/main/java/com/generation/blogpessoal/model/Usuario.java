c8400c82fb3269f90aa185bc91e1a7742537d5d8
Kaellen-heleni <KAELLENHELENI@ICLOUD.COM>
2024-01-03 16:34:30 -0300

    Deploy do Projeto Blog Pessoal utilizando o Render

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -2,20 +2,21 @@ package com.generation.blogpessoal.model;
 
 	import java.util.List;
 
-	import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-	import jakarta.persistence.CascadeType;
-	import jakarta.persistence.Entity;
-	import jakarta.persistence.FetchType;
-	import jakarta.persistence.GeneratedValue;
-	import jakarta.persistence.GenerationType;
-	import jakarta.persistence.Id;
-	import jakarta.persistence.OneToMany;
-	import jakarta.persistence.Table;
-	import jakarta.validation.constraints.Email;
-	import jakarta.validation.constraints.NotBlank;
-	import jakarta.validation.constraints.NotNull;
-	import jakarta.validation.constraints.Size;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 	@Entity
 	@Table(name = "tb_usuarios")
@@ -27,7 +28,8 @@ package com.generation.blogpessoal.model;
 
 		@NotNull(message = "O Atributo Nome é Obrigatório!")
 		private String nome;
-
+		
+		@Schema(example = "email@email.com.br")
 		@NotNull(message = "O Atributo Usuário é Obrigatório!")
 		@Email(message = "O Atributo Usuário deve ser um email válido!")
 		private String usuario;
