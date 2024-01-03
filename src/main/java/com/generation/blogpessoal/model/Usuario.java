4e7dd21e525287228e02def12b2ab3ea88751b1b
Kaellen-heleni <KAELLENHELENI@ICLOUD.COM>
2024-01-03 14:14:17 -0300

    configurando o JUnit 5 no pom.xml

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -92,7 +92,16 @@ package com.generation.blogpessoal.model;
 		public void setPostagem(List<Postagem> postagem) {
 			this.postagem = postagem;
 		}
+		
+		
+		public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+			this.id = id;
+			this.nome = nome;
+			this.usuario = usuario;
+			this.senha = senha;
+			this.foto = foto;
+		}
 
+		public Usuario() { }
 	}
-
 
